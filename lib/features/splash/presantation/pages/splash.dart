45022b857f291cc5d30ba7a278e87760f6ff45fa
akhil babu <85557134+akhilbabu759@users.noreply.github.com>
2023-11-10 16:00:22 +0530

    logo added to assets and splash page created using animated_splash_screen package

---
+++ lib/features/splash/presantation/pages/splash.dart
@@ -0,0 +1,35 @@
+import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'package:antbery/features/auth/presentation/pages/login.dart';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedSplashScreen(
+      splash: SizedBox(
+        height: MediaQuery.of(context).size.height,
+        child: Column(children: [
+          Center(
+            child: Image.asset(
+              'assets/images/antbery logo_bk.png',
+              fit: BoxFit.fill,
+              height: MediaQuery.of(context).size.height * 0.3,
+            ),
+          ),
+          const Text(
+            'antbery.com',
+            style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 30,
+                color: Color.fromARGB(255, 147, 147, 147),
+                fontStyle: FontStyle.italic),
+          )
+        ]),
+      ),
+      nextScreen: const LoginPage(),
+      splashIconSize: MediaQuery.of(context).size.height * 0.5,
+    );
+  }
+}
