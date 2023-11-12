e20fb5e52b6ff1d07600f06d08fdd67d535c3286
akhil babu <85557134+akhilbabu759@users.noreply.github.com>
2023-11-12 18:55:22 +0530

    get_it and injection container added

---
+++ lib/injectin_containerdart.dart
@@ -0,0 +1,10 @@
+import 'package:antbery/config/them/app_style.dart';
+import 'package:antbery/features/home/presentaion/bloc/carousel_bloc/carousel_bloc.dart';
+
+
+import 'package:get_it/get_it.dart';
+final locator = GetIt.instance;
+void setUpLocator() {
+  locator.registerFactory(() => CarouselBloc());
+  locator.registerSingleton(()=>AppStryle()); 
+}
